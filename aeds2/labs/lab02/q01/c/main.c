526a2c259830ec9479dae08ef10e3fff2c74b290
HenriquePkrueger <henriquepimentakrueger@gmail.com>
2026-02-26 23:09:30 -0300

    Adicionado loop para permitir múltiplas entradas

--- aeds2/labs/lab02/q01/c/main.c
+++ aeds2/labs/lab02/q01/c/main.c
@@ -4,33 +4,44 @@ int main() {
 	char s1[100], s2[100], s3[100];
 	int i = 0, j = 0, k = 0;
 
-	printf("Digite a primeira string:\n");
-	scanf("%s", s1);
+	//printf("Digite a primeira string:\n");
+	//scanf("%s", s1);
 
-	printf("Digite a segunda string:\n");
-	scanf("%s", s2);
-
-	while (s1[i] != '\0' && s2[j] != '\0'){
-		s3[k] = s1[i];
-		k++;
-		i++;
-		s3[k] = s2[j];
-		k++;
-		j++;
-	}
-
-	while (s1[i] != '\0'){
-		s3[k] = s1[i];
-		i++;
-		k++;
-	}
+	//printf("Digite a segunda string:\n");
+	//scanf("%s", s2);
 	
-	while (s2[j] != '\0'){
-		s3[k] = s2[j];
-		j++;
-		k++;
+	printf("Digite a primeira e a segunda string:\n");
+	while (scanf("%s %s", s1, s2) != EOF) {
+
+		i = 0;
+		j = 0;
+		k = 0;
+
+		while (s1[i] != '\0' && s2[j] != '\0'){
+			s3[k] = s1[i];
+			k++;
+			i++;
+			s3[k] = s2[j];
+			k++;
+			j++;
+		}
+
+		while (s1[i] != '\0'){
+			s3[k] = s1[i];
+			i++;
+			k++;
+		}
+	
+		while (s2[j] != '\0'){
+			s3[k] = s2[j];
+			j++;
+			k++;
+		}
+	
+	s3[k] = '\0';
+
+	printf("Strings combinadas:\n%s\n", s3);
+
 	}
 
-	s3[k] = '\0';
-	printf("Strings combinadas:\n%s\n", s3);
 }
