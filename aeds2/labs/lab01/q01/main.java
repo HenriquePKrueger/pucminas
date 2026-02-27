49f9889d566039b13d2a221b9695b02f443b8d49
HenriquePkrueger <henriquepimentakrueger@gmail.com>
2026-02-27 20:58:45 -0300

    Adicionado método iterativo para contar letras maiúsculas

--- aeds2/labs/lab01/q01/main.java
+++ aeds2/labs/lab01/q01/main.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
 public class main {
-	public static void main(String[] args){
+
+	public static int contMaiusculas(String s2){
 	
-		Scanner sc = new Scanner(System.in);
-		
-		String s1 = sc.nextLine();
 		int cont = 0;
 
-		for (int i = 0; i < s1.length(); i++) {
-
-			if (s1.charAt(i) >= 65 && s1.charAt(i) <= 90) {
+		for (int i = 0; i < s2.length(); i++) {
+		
+			if (s2.charAt(i) >= 'A' && s2.charAt(i) <= 'Z') {
 
 				cont++;
 
@@ -18,7 +16,25 @@ public class main {
 
 		}
 
-		System.out.println(cont);
+		return cont;
+
+	}
+
+	public static void main(String[] args){
+	
+		Scanner sc = new Scanner(System.in);
+		
+		String s1 = sc.nextLine();
+ 
+		while (!s1.equals("FIM")) {
+
+			int resultado = contMaiusculas(s1);
+
+			System.out.println(resultado);
+
+			s1 = sc.nextLine();
+
+		}
 
 	}
 
