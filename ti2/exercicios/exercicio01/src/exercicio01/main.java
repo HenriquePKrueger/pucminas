718b619af7585fb9abe109ba5956ce355f1c6756
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-02-28 16:27:42 -0300

    Exercicio01 concluído
    Exercício 01 completo

---
+++ ti2/exercicios/exercicio01/src/exercicio01/main.java
@@ -0,0 +1,19 @@
+package exercicio01;
+import java.util.*;
+
+class Main {
+
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1 + num2);
+		
+		sc.close();
+		
+	}
+		
+}
