907a1a957d626020bf50de4fdf896afb311623da
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-03-06 21:25:07 -0300

    Atividade refeita em ambiente 100% Linux

---
+++ ti2/exercicios/exercicio01/src/exercicio01/SomarDoisNumeros.java
@@ -0,0 +1,22 @@
+package exercicio01;
+import java.util.*;
+
+public class SomarDoisNumeros {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1, num2;
+		
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		System.out.println(num1 + num2);
+		
+		sc.close();
+		
+	}
+	
+	
+}
