a515b59c8f19986b160fa1ef60c129a728033287
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-03-09 17:34:03 -0300

    Criada primeira versão do exercício q02 do lab01

--- aeds2/labs/lab01/q02/Main.java
+++ aeds2/labs/lab01/q02/Main.java
@@ -2,13 +2,46 @@ import java.util.*;
 
 public class Main {
 
-	static Scanner sc = new Scanner(System.in);
+	public static int contMaiusculas(String str, int i, int maiusculas){
+
+		if ( i >= str.length() - 1){
+			
+			if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+
+				maiusculas++;
+
+			}
+
+			return maiusculas;
+
+		}
+		else if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z'){
+
+			maiusculas++;
+				
+		}
+
+		i++;
+
+		return contMaiusculas(str, i, maiusculas);
+
+	}
 
 	public static void main(String[] args){
 	
+		Scanner sc = new Scanner(System.in);
+	
+		String s1;
 
+		int indice = 0, result, maiusculas = 0;
 
+		s1 = sc.nextLine();
+
+		result = contMaiusculas(s1, indice, maiusculas);
+
+		System.out.println(result);
+
+		sc.close();
 
 	} 
-
 }
