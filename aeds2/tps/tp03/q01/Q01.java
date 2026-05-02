f0126aef3e7dc3c43fb0e02cba9f56e2fc595535
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-05-01 21:30:22 -0300

    Classe Hora completa

--- aeds2/tps/tp03/q01/Q01.java
+++ aeds2/tps/tp03/q01/Q01.java
@@ -3,7 +3,42 @@ import java.util.*;
 public class Q01{
 	public static void main(String args[]){
 		Data d = Data.parseData("2026-12-27");
-		System.out.println(d.formatar()); 
+		System.out.println(d.formatar());
+		Hora h = Hora.parseHora("11:00");
+		System.out.println(h.formatar());
+	}
+}
+
+class Hora{
+	private int hora;
+	private int minuto;
+		
+	public static Hora parseHora(String s){
+		Hora h = new Hora();
+		
+		String tempHora = "";
+		String tempMinuto = "";
+
+		for(int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+			
+			if(c != ':' && i < 2){
+				tempHora += c;
+			}
+			else if(c != ':'){
+				tempMinuto += c; 
+			}
+		}
+		
+		h.hora = Integer.parseInt(tempHora);
+		h.minuto = Integer.parseInt(tempMinuto);
+		
+		return h;
+	}
+	
+	public String formatar(){
+		String s = String.format("%02d:%02d", this.hora, this.minuto);
+		return s;
 	}
 }
 
