c049db1f96d0ee40fb76f73cbef0ae4453997724
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-05-01 22:49:54 -0300

    Criando a classe Restaurante

--- aeds2/tps/tp03/q01/Q01.java
+++ aeds2/tps/tp03/q01/Q01.java
@@ -6,6 +6,77 @@ public class Q01{
 		System.out.println(d.formatar());
 		Hora h = Hora.parseHora("11:00");
 		System.out.println(h.formatar());
+		Restaurante r = Restaurante.parseRestaurante("1,Classic Palace Works,Zurich,168,3.9,churrasco;internacional,$$,11:00-20:00,2018-03-31,false");
+		System.out.println(r.formatar());
+	}
+}
+
+class Restaurante{
+	private int id;
+	private String nome;
+	private String cidade;
+	private int capacidade;
+	private double avaliacao;
+	private String[] tiposCozinha;
+	private int faixaPreco;
+	private Hora horarioAbertura;
+	private Hora horarioFechamento;
+	private Data dataAbertura;
+	private boolean aberto;
+	
+	public static Restaurante parseRestaurante(String s){
+		Restaurante r = new Restaurante();
+
+		String sTmp = "";
+		int contVirgula = 1;
+		
+		for(int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+			
+			if(c == ','){
+				contVirgula++;
+			}
+		}
+		
+		String[] arrDados = new String[contVirgula];
+		contVirgula = 0;
+
+		for(int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+			
+			if(c != ','){
+				sTmp += c;
+			}
+			else{
+				arrDados[contVirgula] = sTmp;
+				contVirgula++;
+				sTmp = "";
+			}
+		}
+
+		String[] teste = new String[2];
+		teste[0] = "teste1;";
+		teste[1] = " teste2";		
+
+		r.id = Integer.parseInt(arrDados[0]);
+		r.nome = arrDados[1];
+		r.cidade = arrDados[2];
+		r.capacidade = Integer.parseInt(arrDados[3]);
+		r.avaliacao = Double.parseDouble(arrDados[4]);
+		r.tiposCozinha = teste;//Separar arrDados[5]
+		r.faixaPreco = arrDados[6].length();
+		r.horarioAbertura = Hora.parseHora("11:00");//Separar arrDados[7]
+		r.horarioFechamento = Hora.parseHora("23:59");
+		r.dataAbertura = Data.parseData(arrDados[8]);
+		r.aberto = Boolean.parseBoolean(arrDados[9]);
+
+		return r;			
+	}
+
+	public String formatar(){
+		String s = String.format("[%d ## %s ## %s ## %d ## %.2f ## %s ## %d ## %s-%s ## %s ## %b]", this.id, this.nome, this.cidade, this.capacidade, this.avaliacao,
+		this.tiposCozinha, this.faixaPreco, this.horarioAbertura, this.horarioFechamento, this.dataAbertura, this.aberto);
+		return s;
 	}
 }
 
@@ -16,22 +87,22 @@ class Hora{
 	public static Hora parseHora(String s){
 		Hora h = new Hora();
 		
-		String tempHora = "";
-		String tempMinuto = "";
+		String tmpHora = "";
+		String tmpMinuto = "";
 
 		for(int i = 0; i < s.length(); i++){
 			char c = s.charAt(i);
 			
 			if(c != ':' && i < 2){
-				tempHora += c;
+				tmpHora += c;
 			}
 			else if(c != ':'){
-				tempMinuto += c; 
+				tmpMinuto += c; 
 			}
 		}
 		
-		h.hora = Integer.parseInt(tempHora);
-		h.minuto = Integer.parseInt(tempMinuto);
+		h.hora = Integer.parseInt(tmpHora);
+		h.minuto = Integer.parseInt(tmpMinuto);
 		
 		return h;
 	}
@@ -50,27 +121,27 @@ class Data{
 	public static Data parseData(String s){
 		Data d = new Data();
 
-		String tempAno = "";
-		String tempMes = "";
-		String tempDia = "";
+		String tmpAno = "";
+		String tmpMes = "";
+		String tmpDia = "";
 		
 		for(int i = 0; i < s.length(); i++){
 			char c = s.charAt(i);
 			
 			if(c != '-' && i < 4){
-				tempAno += c;	
+				tmpAno += c;	
 			}
 			else if(c != '-' && i < 7){
-				tempMes += c;
+				tmpMes += c;
 			}
 			else if(c != '-'){
-				tempDia += c;
+				tmpDia += c;
 			}
 		}
 
-			d.ano = Integer.parseInt(tempAno);
-			d.mes = Integer.parseInt(tempMes);
-			d.dia = Integer.parseInt(tempDia);
+			d.ano = Integer.parseInt(tmpAno);
+			d.mes = Integer.parseInt(tmpMes);
+			d.dia = Integer.parseInt(tmpDia);
 
 		return d;
 	}
