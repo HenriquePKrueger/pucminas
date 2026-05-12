c0520c774a04338a600e12d5868cdfc504ea9797
Henrique Pimenta Krueger <henriquepimentakrueger@gmail.com>
2026-05-12 15:36:49 -0300

    Exercício em java concluído

--- aeds2/verde/concurso-de-peso/java/Concurso.java
+++ aeds2/verde/concurso-de-peso/java/Concurso.java
@@ -1,75 +1,67 @@
 import java.util.*;
 
-/*
- * Seu programa deverá ler os dados dos atletas com nome (sem espaços) e o peso máximo levantado (inteiro positivo), e apresentar a listagem dos atletas a partir dos maiores pesos levantados.
- * Em caso de empate no peso, ordenar em ordem alfabética pelo nome. Seu programa deve ler os atletas até o fim do arquivo. Imprimir os atletas um por linha no formato: Nome Peso.
- * 
- * Regras e Restrições:
- *
- * Criar uma estrutura de dados para representar um atleta.
- * O número de atletas será no máximo 100.
- * O nome de cada atleta não terá espaços e terá no máximo 50 caracteres.
- * O peso máximo será um número inteiro entre 1 e 500.
-*/
-
-
 public class Concurso{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		ListaSequencial l = new ListaSequencial();
-		Selecao s = new Selecao();
-
-		while(sc.hasNextLine()){
+		Lista l = new Lista();	
+	
+		while(sc.hasNext()){
 			String nome = sc.next();
-			int peso = sc.nextInt();
-			
+			int peso = sc.nextInt();	
+	
 			l.inserir(nome, peso);
-			s.ordenar();
 		}
+		
+		l.selecao();
+		l.exibir();
+		sc.close();
 	}
 }
 
-class ListaSequencial{
+class Lista{
 	Atleta[] atletas = new Atleta[100];
-	int pos = 0;
+	int index = 0;
 
 	public void inserir(String nome, int peso){
-		atletas[pos] = new Atleta(nome, peso);
-		pos++;
+		Atleta novo = new Atleta();
+		novo.nome = nome;
+		novo.peso = peso;
+		atletas[index] = novo;
+		index++;
 	}
-}
 
-class Selecao{
-	public int tamanho = 100;
+	public void selecao(){
+		for(int i = 0; i < index; i++){
+			int maior = i;
 
-	public void ordenar(){
-		for(int i = 0; i < this.tamanho; i++){
-			int menor = i;
-	
-			for(int j = i + 1; j < this.tamanho; j++){
-				if(ListaSequencial.atletas[j] < ListaSequecial.atletas[menor]){
-					menor = ListaSequencial.atletas[j];
+			for(int j = i + 1; j < index; j++){
+				if(atletas[j].peso > atletas[maior].peso){
+					maior = j;
 				}
+				else if(atletas[j].peso == atletas[maior].peso){//Condição de desempate
+					if(atletas[j].nome.compareTo(atletas[maior].nome) < 0){
+						maior = j;
+					}
+				}
+				
 			}
+
+			Atleta temp = atletas[i];
+			atletas[i] = atletas[maior];
+			atletas[maior] = temp;			
 		}
 	}
 
-	public void swap(Atleta j, int menor){
-		Atleta temp = ListaSequencial.atletas[i];
-		ListaSequencial.atletas[i] = ListaSequencial.atletas[menor];
-		ListaSequencial.atletas[menor] = temp;
+	public void exibir(){
+		for(int i = 0; i < index; i++){
+			System.out.print(atletas[i].nome + " ");
+			System.out.println(atletas[i].peso);
+		}
 	}
-
-
 }
 
 class Atleta{
-	String nome;
-	int peso;
-
-	public Atleta(String nome, int peso){
-		this.nome = nome;
-		this.peso = peso;
-	}
-
+	String nome = "";
+	int peso = 0;
 }
+
